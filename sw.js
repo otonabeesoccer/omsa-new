aa6e13ae91c5e34a81c5d7e4cfc8ee85f5625607
otonabeesoccer <46427054+otonabeesoccer@users.noreply.github.com>
2019-03-19 18:36:08 -0400

    removed commented lines from html, added sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/styles.css`,
+        `/bootstrap.min.css`,
+        `/bg-grass.png`,
+        `/2019_OMSA_REGISTRATION.pdf`,
+        `/scripts/pwacompat.min.js`,
+        `/sounds/airhorn.mp3`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
